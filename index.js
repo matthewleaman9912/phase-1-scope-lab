661bec325db41a003100c15c3b038c71d7d35706
MatthewLeaman <leaman.30@osu.edu>
2023-12-18 19:21:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(name) {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(name) {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Arlo";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Bert";
+    return leastFavoriteCustomer;
+}
